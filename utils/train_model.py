7416c3478474b6b92736f90df483854dd75e316c
alexisvannson <vannson.alexis@gmail.com>
2025-08-22 11:32:10 +0200

    added load weights and changed output management

--- utils/train_model.py
+++ utils/train_model.py
@@ -2,14 +2,21 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import os
+import time
 
-def train(model, dataset, epochs, method="", patience=5, output_path='weights'):
+def train(model, dataset, epochs, method="", patience=5, output_path='weights', start_weights=None):
 	optimizer = optim.Adam(model.parameters(), lr=1e-3)
 	criterion = nn.CrossEntropyLoss()
 	best_loss = float('inf')
 	patience_counter = 0
 	
+	if start_weights:
+		model.load_state_dict(torch.load(start_weights))
+	
+	os.makedirs(output_path, exist_ok=True)
+
 	for epoch in range(epochs):
+		checkpoint1 = time.time()
 		epoch_loss = 0
 		num_batches = 0
 		for sample, label in dataset:
@@ -31,12 +38,16 @@ def train(model, dataset, epochs, method="", patience=5, output_path='weights'):
 		
 		avg_loss = epoch_loss / max(1, num_batches)
 		print(f"Epoch {epoch+1}/{epochs}, avg_loss={avg_loss:.4f}")
+		checkpoint2 = time.time()
+		print(f"epoch: {epoch} needed {checkpoint2 - checkpoint1} time")
+		with open(os.path.join('output_pth', 'trainning_logs.txt'), "a") as the_file:
+			the_file.write(f"Epoch {epoch+1}/{epochs}, avg_loss={avg_loss:.4f}\n")
+			the_file.write(f"Epoch {epoch+1}/{epochs}, needed {(checkpoint2 - checkpoint1) / 60:.2f} minutes\n")
 		
 		# Early stopping
 		if avg_loss < best_loss:
 			best_loss = avg_loss
 			patience_counter = 0
-			os.makedirs(output_path, exist_ok=True)
 			torch.save(model.state_dict(), os.path.join(output_path, f'best_model_{method}_epoch{epoch+1}.pth'))
 		else:
 			patience_counter += 1
@@ -44,5 +55,5 @@ def train(model, dataset, epochs, method="", patience=5, output_path='weights'):
 		if patience_counter >= patience:
 			print(f"Early stopping at epoch {epoch+1}")
 			break
-	os.makedirs(output_path, exist_ok=True)
+		
 	torch.save(model.state_dict(), os.path.join(output_path, f'final_model_{method}.pth'))
